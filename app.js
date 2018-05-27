40167002ec424af2723cc09db820cbeecfba6b93
sjillen <thomas.proust89@gmail.com>
2018-05-27 20:39:51 +0900

    basic files and packages installed, express test implemented

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+/* Request Handlers */
+
+// Watch for incoming requests of method GET
+// to the route http://localhost:3050/api
+app.get('/api', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+module.exports = app;
